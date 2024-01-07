96152f1b74da9dcbc03a743bebb391f63742e669
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2024-01-07 18:27:58 +0700

    feat : scan activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,12 @@ android {
     namespace = "com.example.scanocular"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+
+    }
+
+
     defaultConfig {
         applicationId = "com.example.scanocular"
         minSdk = 26
@@ -36,12 +42,26 @@ android {
 }
 
 dependencies {
+    val camerax_version = "1.4.0-alpha03"
+
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+
 }
