f2dce18dc21ccf7bbfdacdd6bf3525eaa7535d01
pudyasta <pudyastasatria@gmail.com>
2024-01-09 21:20:25 +0700

    feat(login and register)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
--- app/src/main/java/com/example/scanocular/util/RetrofitClient.kt
+++ app/src/main/java/com/example/scanocular/util/RetrofitClient.kt
@@ -4,17 +4,21 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-
+import okhttp3.logging.HttpLoggingInterceptor
 object RetrofitClient {
     val instance: Retrofit by lazy {
+        val mHttpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
         val client = OkHttpClient.Builder()
             .readTimeout(60, TimeUnit.SECONDS)   // Increase read timeout to 60 seconds
             .connectTimeout(60, TimeUnit.SECONDS) // Increase connect timeout to 60 seconds
-            .writeTimeout(60, TimeUnit.SECONDS)   // Increase write timeout to 60 seconds
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(mHttpLoggingInterceptor)
             .build()
 
         Retrofit.Builder()
-            .baseUrl("https://scanocular.online/")
+            .baseUrl("https://scanocular.online/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
